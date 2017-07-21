5753cac39b3a4aa62d41fbf7dd679f2dd4810b0f
eceris <eceris@daou.co.kr>
2017-07-21 23:01:31 +0900

    formatting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var url = require('url');
 var ROOT_DIR = "./";
 var fs = require('fs');
 
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function (request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     // Url {
     //     protocol: null,
@@ -36,37 +36,34 @@ var server = http.createServer(function(request, response) {
     }
 });
 
-var htmlForLeader = function(response) {
-    fs.readFile(ROOT_DIR + '/leader.html',function (err,data) {
-        if (err) {
-            response.writeHead(404);
-            response.end(JSON.stringify(err));
-            return;
-        }
-        response.writeHead(200);
-        response.end(data);
-    });
-};
-
-var htmlForGuest = function(response) {
-    fs.readFile(ROOT_DIR + '/guest.html',function (err,data) {
-        if (err) {
-            response.writeHead(404);
-            response.end(JSON.stringify(err));
-            return;
-        }
-        response.writeHead(200);
-        response.end(data);
-    });
-};
-
-server.listen(8080, function() {
+server.listen(8080, function () {
     console.log((new Date()) + ' Server is listening on port 8080');
 
 });
 
+var htmlForLeader = function (response) {
+    fs.readFile(ROOT_DIR + '/leader.html', function (err, data) {
+        if (err) {
+            response.writeHead(404);
+            response.end(JSON.stringify(err));
+            return;
+        }
+        response.writeHead(200);
+        response.end(data);
+    });
+};
 
-
+var htmlForGuest = function (response) {
+    fs.readFile(ROOT_DIR + '/guest.html', function (err, data) {
+        if (err) {
+            response.writeHead(404);
+            response.end(JSON.stringify(err));
+            return;
+        }
+        response.writeHead(200);
+        response.end(data);
+    });
+};
 
 
 //////////////////////////////////////////////////ws part///////////////////////////////////////////
@@ -78,31 +75,34 @@ wsServer = new WebSocketServer({
 function originIsAllowed(origin) {
     return true;
 }
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
     if (!originIsAllowed(request.origin)) { // Make sure we only accept requests from an allowed origin request.reject();
         console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
         return;
     }
     var connection = request.accept(null, request.origin);
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
+    connection.on('message', function (message) {
         if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
             debugger;
             var msg = JSON.parse(message.utf8Data);
+
+
             // send message to client
             connection.sendUTF(JSON.stringify({
                 id: msg.id,
-                event : null,
-                message : 'Hi client, I have got a message from you : ' + msg.message
+                event: null,
+                message: 'Hi client, I have got a message from you : ' + msg.message
             }));
-        } else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
         }
     });
-    connection.on('close', function(reasonCode, description) {
+    connection.on('close', function (reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+
+var determineEventAndBroadcast = function () {
+
+};
 //////////////////////////////////////////////////ws part///////////////////////////////////////////
